402f4b3efea176db8205d4b885ee66014ec4edf3
Pmaioranool <lucasmaiorano77@gmail.com>
2025-02-05 18:19:42 +0100

    initialize Vue 3 project with TypeScript and Vite setup

---
+++ src/Page/HomePage.vue
@@ -0,0 +1,7 @@
+<template>
+  <main>
+    <h1>Home page</h1>
+  </main>
+</template>
+<script setup lang="ts"></script>
+<style></style>
---
+++ src/Page/LoginPage.vue
@@ -0,0 +1,8 @@
+<template>
+  <h1>Login page</h1>
+</template>
+<script setup lang="ts">
+import FieldComponent from "./components/FieldComponent.vue";
+import ButtonComponent from "./components/ButtonComponent.vue";
+</script>
+<style></style>
