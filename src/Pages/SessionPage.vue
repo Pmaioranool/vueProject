c5456e81bad7ef88bcd2fd508cf88e3d0a1418df
Pmaioranool <lucasmaiorano77@gmail.com>
2025-02-12 18:31:36 +0100

    feat: add route parameter logging in SessionPage component

--- src/Pages/SessionPage.vue
+++ src/Pages/SessionPage.vue
@@ -3,3 +3,12 @@
     <h1>Session page</h1>
   </main>
 </template>
+<script setup lang="ts">
+import { onMounted } from "vue";
+import { useRoute } from "vue-router";
+
+onMounted(() => {
+  const route = useRoute();
+  console.log(route.params);
+});
+</script>
