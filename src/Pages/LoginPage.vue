8196676d9ff2d0193cc8fe0ed2bb21a5f683329a
Pmaioranool <lucasmaiorano77@gmail.com>
2025-02-12 16:35:33 +0100

    fix: validate user input in LoginPage and log validation results

--- src/Pages/LoginPage.vue
+++ src/Pages/LoginPage.vue
@@ -95,10 +95,11 @@ const inputHandler = () => {};
 
 const isUserInputValid = (input: string): boolean => {
   const pattern = new RegExp(/[A-Z0-9._%+-]+@[A-Z0-9-]+.+.[A-Z]{2,4}/i);
-  return false;
+  return pattern.test(input);
 };
 
 watch(logUser, (val) => {
-  console.log("change");
+  console.log(val.username);
+  console.log(isUserInputValid(val.username));
 });
 </script>
